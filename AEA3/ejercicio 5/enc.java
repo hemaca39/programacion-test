1905ceb890f252bbc8791d6f10ab82f67235a6cc
hemaca39 <hemaca@pratfp.com>
2026-02-01 22:54:54 +0100

    encarrec

---
+++ AEA3/ejercicio 5/enc.java
@@ -0,0 +1,11 @@
+public class enc {
+    private int dia;
+    private int mes;
+    private int hora;
+    private int minuts;
+    
+    public void afegirProducte(Peticio p) {
+    }
+    public void eliminarProducte(Peticio p) {
+    }
+}
