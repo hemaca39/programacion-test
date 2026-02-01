0f40c458f621b2b795f7b9b3c6fa9536c89e1cce
hemaca39 <hemaca@pratfp.com>
2026-02-01 22:53:13 +0100

    transportista

---
+++ AEA3/ejercicio 5/transpoortista.java
@@ -0,0 +1,13 @@
+public class transpoortista {
+    private int numLlicencia;
+    
+    private String nom;
+    private String telefon;
+      
+
+    public void assignarEncarrec(Encarrec e) {
+    }
+
+    public void anul·larEncarrec(Encarrec e) {
+    }
+}
