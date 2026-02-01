d3f141ed2130a49cf5838495363a2628cbea9ac3
hemaca39 <hemaca@pratfp.com>
2026-02-01 22:59:21 +0100

    client

---
+++ AEA3/ejercicio 5/cli.java
@@ -0,0 +1,13 @@
+public class Client {
+    private String email;
+    private String adreça;
+    private String nom;
+    private String telefon;
+ 
+
+    public void assignarEncarrec(Encarrec e) {
+    }
+
+    public void anullarEncarrec(Encarrec e) {
+    }
+}
