68dfe1acbf7434ddad2013bc23c0b2dc7d63eb20
hemaca39 <hemaca@pratfp.com>
2026-03-04 18:35:39 +0100

    cercle

---
+++ AEA3/ejercicio 7/cercle.java
@@ -0,0 +1,11 @@
+public class cercle extends Figura {
+    private int radi;
+
+    public cercle(int r) {
+        this.radi = r;
+    }
+
+    public void calcularArea() {
+        this.area = Math.PI * Math.pow(radi, 2);
+    }
+}
