1c0e8cc4a1e016dd89ceb31bc4b169a5dda49dfb
hemaca39 <hemaca@pratfp.com>
2026-03-23 20:34:33 +0100

    corr

--- AEA3/Examenbueno/NotaAText.java
+++ AEA3/Examenbueno/NotaAText.java
@@ -1,7 +1,7 @@
 public class NotaAText {
     public String notaAText(double nota) {
         if (nota < 5) return "Suspés";
-        if (nota < 7) return "Aprovat";
+        if (nota < 6) return "Aprovat";
         if (nota < 9) return "Notable";
         return "Excel·lent";
     }
