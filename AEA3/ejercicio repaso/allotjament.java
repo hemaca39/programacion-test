31d820460a2c2f9ab0c2abd9e2de331adaf3276f
hemaca39 <hemaca@pratfp.com>
2026-03-23 17:33:28 +0100

    allo

---
+++ AEA3/ejercicio repaso/allotjament.java
@@ -0,0 +1,33 @@
+public abstract class allotjament {
+    protected String nom;
+    protected int capacitat;
+    protected boolean disponible;
+
+    public Allotjament(String nom, int capacitat, boolean disponible) {
+        this.nom = nom;
+        this.capacitat = capacitat;
+        this.disponible = disponible;
+    }
+
+    public abstract double calcularPreuPerNit();
+
+    public String mostrarInformacio() {
+        return "Nom: " + nom + ", Capacitat: " + capacitat + ", Disponible: " + disponible;
+    }
+
+    public boolean reservar() {
+        if (disponible) {
+            this.disponible = false;
+            return true;
+        }
+        return false;
+    }
+
+    public void alliberar() {
+        this.disponible = true;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+}
